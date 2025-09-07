faeb8749937eb6e82977d26228d897f14256df2a
Zarina Panfilova <zhiyenbayeva.z@gmail.com>
2025-09-08 04:08:55 +0500

    feat: resolve the task

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/*
 const MORSE_TABLE = {
   '.-': 'a',
   '-...': 'b',
@@ -37,8 +36,28 @@ const MORSE_TABLE = {
   '----.': '9',
   '-----': '0',
 };
-*/
 
-module.exports = function decode(/* expr */) {
-  throw new Error('Not implemented');
+module.exports = function decode(expr) {
+  let result = '';
+
+  for (let i = 0; i < expr.length; i += 10) {
+    const chunk = expr.slice(i, i + 10);
+
+    if (chunk === '**********') {
+      result += ' ';
+    } else {
+      const code = chunk.replace(/^0+/, '');
+
+      let morse = '';
+      for (let j = 0; j < code.length; j += 2) {
+        const pair = code.slice(j, j + 2);
+        if (pair === '10') morse += '.';
+        else if (pair === '11') morse += '-';
+      }
+
+      result += MORSE_TABLE[morse] || '';
+    }
+  }
+
+  return result;
 };
